4681a8094c55632695142811162134770777ca8d
Ryan Kibler <rdkibler@gmail.com>
2023-08-09 17:45:42 -0700

    final_setup

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ The python environment used to run the scripts and ipython notebooks is specifie
 Install [DeepAccNet](https://github.com/hiranumn/DeepAccNet)
 
 
+To run the pipeline, pick either BGL or RTR as the example path to follow. Under both paths, the pipeline is run by visiting, adjusting, and running the scripts in the numbered subdirs. In most cases, the `gentasks.py` or `gentasks.ipynb` files both provide examples of how to run the scripts as well as prepare a list of tasks to be run. Like any software downloaded from the internet, the scripts may need to be adjusted to run on your system. That may includes paths to software, paths to input files, and paths to output files.
+
+
+Note that several scripts depend on `ss_grouped_vall_helix_shortLoop.h5`. This file is not included in this repository due to its large size and is available upon request. 
+
+
+
--- RTR/03_design/run/run_filters.py
+++ RTR/03_design/run/run_filters.py
@@ -54,14 +54,7 @@ import glob
 import os
 
 net_num = input_pdb.split("_")[-1].split(".")[0].lstrip("0")
-#try :
-#	cst_file = glob.glob(f"{os.path.dirname(input_pdb)}/*_hb_network_{net_num}.cst")[0]
-#except:
-#	cst_file = f"{os.path.splitext(input_pdb)[0]}_hb_network_1.cst"
 
-
-#these constraints are written to the pdb so I can actually turn off the cst write!
-#now I can pre-filter
 constraints=[]
 HBNet_resis = []
 with open(input_pdb,'r') as pdb:
@@ -73,23 +66,6 @@ with open(input_pdb,'r') as pdb:
 constraints_str = "".join(constraints)
 #print(constraints_str)
 
-#descor = "/home/rdkibler/projects/tiara_gen2/toroids/05_2_ctor-24x_parametric/03_generate_hbnets/design_correspondance.json"
-#with open(descor,'r') as f:
-#	connection_lookup = json.load(f)
-
-#if connection_lookup[os.path.basename(input_pdb)].split("/")[6].split("_")[-1] == "C":
-#	terminal_helix_selection_start='-2,H,M'
-#	terminal_helix_selection_stop='c_term'
-#elif connection_lookup[os.path.basename(input_pdb)].split("/")[6].split("_")[-1] == "N":
-#	terminal_helix_selection_start='n_term'
-#	terminal_helix_selection_stop='2,H,M'
-#else:
-#	print("NOT RECOGNIZED:")
-#	print(connection_lookup[os.path.basename(input_pdb)].split("/")[6].split("_")[-1])
-
-#print("might need reworking")
-#exit()
-
 
 #The CST files Scott writes are actually not functional (yay) so we gotta make a temp file
 import tempfile
@@ -99,10 +75,7 @@ with tempfile.NamedTemporaryFile() as temp:
 	temp.flush()
 
 	suffix = f"_designed"
-	#command=f"/home/bcov/ppi/builds/19-09-18/main/source/cmake/build_release_omp_hdf5/rosetta_scripts " \
-	#	f"-holes:dalphaball /home/bcov/ppi/tutorial_build/main/source/external/DAlpahBall/DAlphaBall.gcc " \
-
-#	command=f"/net/software/rosetta/versions/v2020.25-dev61318/main/source/build/src/release/linux/5.4/64/x86/gcc/9/hdf5/rosetta_scripts.hdf5.linuxgccrelease " \
+	
 	command=f"/net/software/rosetta/versions/v2020.50-dev61505/main/source/build/src/release/linux/5.4/64/x86/gcc/9/hdf5/rosetta_scripts.hdf5.linuxgccrelease " \
 		f"-corrections::beta_nov16 true " \
 		f"-indexed_structure_store:fragment_store /home/brunette/DBs/hdf5/ss_grouped_vall_helix_shortLoop.h5 " \
